6a14f613a2e8099c36a3620f3e7b41022f858117
uysseetienne <ulysse.etienne9@gmail.com>
2025-03-13 12:14:05 -0400

    labeled each stat gauge

--- src/Panels/StatsPanel.java
+++ src/Panels/StatsPanel.java
@@ -8,31 +8,38 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.util.Scanner;
 
 public class StatsPanel extends JPanel {
     private int[] stats;
     private Part character,vehicle,tires,glider;
     private BufferedImage[] gauges;
-    private JLabel[] labels;
-
+    private String[] statNames;
     public StatsPanel(Part character,Part vehicle,Part tires,Part glider){
         stats = new int[14];
         gauges = new BufferedImage[21];
+        statNames = new String[14];
         try{
+            Scanner sc = new Scanner(new FileInputStream("src/InitFiles/statNames.txt"));
             for(int i =0;i<21;i++){
                 gauges[i] = ImageIO.read(new File("src/images/gauge/gauge"+i+".png"));
+            }
+            for(int i=0;i<14;i++){
+                statNames[i] = sc.nextLine();
             }
         }catch(Exception ioe){
             ioe.printStackTrace();
         }
+
         this.character = character;
         this.vehicle = vehicle;
         this.tires = tires;
         this.glider = glider;
         setStats();
         this.setVisible(true);
-        this.setBounds(800,200,400,400);
+        this.setBounds(750,150,500,400);
     }
 
 
@@ -65,8 +72,10 @@ public class StatsPanel extends JPanel {
     //paints the gauges
     @Override
     public void paintComponent(Graphics g){
+        g.setColor(Color.white);
         for(int i = 0;i<14;i++){
-            g.drawImage(gauges[stats[i]],0,(i*28),this);
+            g.drawString(statNames[i],0,i*28+15);
+            g.drawImage(gauges[stats[i]],120,(i*28),this);
         }
     }
 }
